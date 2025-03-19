4bbaeb7bd70f370690c401669a120b22dc37c60b
nickelodeon07 <160284236+nickelodeon07@users.noreply.github.com>
2025-03-18 23:23:20 -0300

    Delete config directory

--- config/dbConnection.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-module.exports = function (){
-    return mysql.createConnection({
-        host: 'localhost',
-        user: 'ifms',
-        password: 'ifms2024!',
-        database: 'portal_noticias'
-    });
-}
--- config/server.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var app = express();
-app.set('view engine','ejs'); //informa ao express que serpa usado ejs como engrenagem das views
-app.set('views','./app/views'); // indica o caminho das viwes
-
-module.exports = app;
